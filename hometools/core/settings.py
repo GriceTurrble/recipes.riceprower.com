772a219aab19459a0b0cd2eff529dacecf052659
Galen Rice <galen.rice@atumbomb.com>
2020-09-05 22:17:06 -0400

    Fix static_root

--- hometools/core/settings.py
+++ hometools/core/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [(BASE_DIR / "static")]
+STATIC_ROOT = BASE_DIR / "static"
 
 
 # Media files (uploads and such)

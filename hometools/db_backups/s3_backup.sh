a631ef2a394e180263578a955cf78b96be5b088c
Galen Rice <galen.rice@atumbomb.com>
2020-10-29 01:23:42 -0400

    Seems I need to include the user role. Done.

--- hometools/db_backups/s3_backup.sh
+++ hometools/db_backups/s3_backup.sh
@@ -5,12 +5,13 @@ source config.sh
 # Config fills in:
 #   $S3_BUCKET - s3 bucket name, in format `s3://bucket-name`
 #   $PG_DATABASE - name of the database to dump, i.e. `mydatabase`
+#   $PG_USER - user that will connect to make the backup.
 
 # Dump database backup to a file
 TIME=$(date "+%s")
 BACKUP_FILE="postgres_${PG_DATABASE}_${TIME}.pgdump"
 echo "Backing up $PG_DATABASE to $BACKUP_FILE"
-pg_dump --format=custom > $BACKUP_FILE
+pg_dump --username $PG_USER --no-password --format=custom > $BACKUP_FILE
 
 # Copy file to AWS S3 using awscli
 # Don't have awscli? Run `sudo apt-get install awscli` to install,

7f48d6bc3992c54f1c51368ddd063261018c660b
Galen Rice <galen.rice@atumbomb.com>
2020-10-29 01:30:01 -0400

    Change back to not needing a user

--- hometools/db_backups/s3_restore.sh
+++ hometools/db_backups/s3_restore.sh
@@ -5,7 +5,6 @@ source config.sh
 # Config fills in:
 #   $S3_BUCKET - s3 bucket name, in format `s3://bucket-name`
 #   $PG_DATABASE - name of the database to dump, i.e. `mydatabase`
-#   $PG_USER - user that will connect to make the backup.
 
 echo -e "\nRestoring database $PG_DATABASE from S3 backups"
 
@@ -16,5 +15,5 @@ echo -e "\nFound file $LATEST_FILE in bucket $S3_BUCKET"
 # Restore from the latest backup file
 echo -e "\nRestoring $PG_DATABASE from $LATEST_FILE"
 S3_TARGET=$S3_BUCKET/$LATEST_FILE
-aws s3 cp $S3_TARGET - | pg_restore --username $PG_USER --no-password --dbname $PG_DATABASE --clean --no-owner
+aws s3 cp $S3_TARGET - | pg_restore --dbname $PG_DATABASE --clean --no-owner
 echo -e "\nRestore completed"

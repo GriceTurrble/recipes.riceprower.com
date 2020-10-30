024a2b1d2673d10a909b6b33949439667b100573
Galen Rice <galen.rice@atumbomb.com>
2020-10-30 15:21:24 -0400

    Add a deployment script
    Doesn't fit well as a management command, so leaving here.
    
    gitignore similar to the config.sh pattern for db backups,
    with a template included here to copy and adjust on the server.
    
    Deployment should be relatively straightforward, checking out code,
    running collectstatic and migrate,
    and restarting services.
    
    May need to add a flag to do pip installs
    if there are changes in the requirements,
    or just run it blind. Not sure how I want to handle that yet.

---
+++ hometools/scripts/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Force sudo if not set
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+
+CONFIG_FILE=$(realpath config.sh)
+
+if [ -f "$CONFIG_FILE" ]
+
+source $CONFIG_FILE
+# Adds:
+#   $MANAGE_DIR - full path to directory where manage.py can be found
+#   $ENV_ACTIVATE - path to the "activate" script for the venv for the project
+
+cd $MANAGE_DIR
+
+echo "[DEPLOY] pulling changes from Git remote..."
+git checkout -f main
+git pull
+
+echo "[DEPLOY] Activating venv..."
+source $ENV_ACTIVATE
+echo "[DEPLOY] Collecting static files..."
+python manage.py collectstatic --clear --noinput
+echo "[DEPLOY] Migrating DB changes..."
+python manage.py migrate --noinput
+echo "[DEPLOY] Deactivating venv..."
+deactivate
+
+echo "[DEPLOY] Restarting services..."
+systemctl daemon-reload
+systemctl restart gunicorn
+systemctl restart nginx
+echo "[DEPLOY] Complete."

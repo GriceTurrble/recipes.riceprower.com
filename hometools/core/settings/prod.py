612cf6ba42c87f0b8ebb8a3249cd64e5f962871b
Galen Rice <galen.rice@atumbomb.com>
2020-10-30 12:00:50 -0400

    prod log file adjustments
    Trying to get request info logging, while also
    adding in the securtity loggers just in case.

--- hometools/core/settings/prod.py
+++ hometools/core/settings/prod.py
@@ -6,13 +6,22 @@ Any settings defined here can override base settings implicitly.
 from .base import LOGS_ROOT
 
 LOGGING_HANDLERS = {
-    # Server access logs from django.request
-    "access_file": {
+    # Logs from django.request
+    "requests_file": {
         "level": "INFO",
         "filters": ["require_debug_false"],
         "class": "logging.handlers.RotatingFileHandler",
         "maxBytes": 1024 * 1024 * 10,  # 10MB
-        "filename": str(LOGS_ROOT / "access.log"),
+        "filename": str(LOGS_ROOT / "requests.log"),
+        "formatter": "verbose",
+    },
+    # Logs from django.security
+    "security_file": {
+        "level": "INFO",
+        "filters": ["require_debug_false"],
+        "class": "logging.handlers.RotatingFileHandler",
+        "maxBytes": 1024 * 1024 * 10,  # 10MB
+        "filename": str(LOGS_ROOT / "security.log"),
         "formatter": "verbose",
     },
     # Other general INFO messages in a main log
@@ -24,6 +33,9 @@ LOGGING_HANDLERS = {
         "filename": str(LOGS_ROOT / "main.log"),
         "formatter": "verbose",
     },
+    "null": {
+        "class": "logging.NullHandler",
+    },
 }
 LOGGING_LOGGERS = {
     # Catch all in the main log if not configured otherwise
@@ -31,10 +43,21 @@ LOGGING_LOGGERS = {
         "handlers": ["file"],
         "propagate": True,
     },
-    # Send server access logs to its own file
-    "django.server": {
-        "handlers": ["access_file"],
+    # Send request access logs to its own file
+    "django.request": {
+        "handlers": ["requests_file"],
         "level": "INFO",
+        "propagate": False,
+    },
+    # Send security logs to its own file
+    "django.security": {
+        "handlers": ["security_file"],
+        "level": "INFO",
+        "propagate": False,
+    },
+    # Ignore disallowed host errors
+    "django.security.DisallowedHost": {
+        "handlers": ["null"],
         "propagate": False,
     },
 }

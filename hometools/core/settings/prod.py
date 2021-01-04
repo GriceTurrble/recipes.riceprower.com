ff93ec71606b111bcf161bcd8f3959f3049d5a60
Galen Rice <galen.rice@atumbomb.com>
2021-01-04 14:16:40 -0500

    Add security checklist items to prod settings.
    Initial 1 hour test for HSTS setting, to be increased to 1 year after prod test.

--- hometools/core/settings/prod.py
+++ hometools/core/settings/prod.py
@@ -50,3 +50,10 @@ LOGGING_LOGGERS = {
         "propagate": False,
     },
 }
+
+
+# Additional security settings
+SECURE_HSTS_SECONDS = 3600
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True

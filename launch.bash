2b00549da330218ea05547eab101d05194f7d678
Galen Rice <galen.rice@atumbomb.com>
2021-03-06 23:04:10 -0500

    Adding launch script to streamline startups

---
+++ launch.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Detects if script are not running as root
+if [ "$UID" != "0" ]; then
+    if whereis sudo &>/dev/null; then
+        # Re-run this script using sudo, then exit
+        # $0 is the script itself (or the command used to call it)
+        # $@ adds all other arguments
+        # The -k option forces the sudo password on every attempt
+        # by removing the timeout from any prior sudo call
+        # (little extra security)
+        exec sudo -k $0 $@
+        exit
+    else
+        echo "Sudo not found. You will need to run this script as root."
+        exit
+    fi
+fi
+
+DIR="$(dirname "$(readlink -f "$0")")"
+COMPOSE_FILE=$DIR/docker-compose.yml
+APP_MANAGE_PY=/app/recipesite/manage.py
+
+# Bring services up
+docker-compose -f $COMPOSE_FILE up
+
+# Migrate database changes
+docker-compose -f $COMPOSE_FILE exec web poetry run python $APP_MANAGE_PY migrate --noinput
+# Collect static files
+docker-compose -f $COMPOSE_FILE exec web poetry run python $APP_MANAGE_PY collectstatic --clear --noinput
+
+# Restart Nginx
+systemctl restart nginx

1e21baf4c86f52b02d00e25e73236867d73406a5
Galen Rice <galen.rice@atumbomb.com>
2021-10-28 15:06:23 -0400

    fix: [partial] Using django-environ for env settings
    This is incomplete, but was enough to get me started.

--- recipesite/core/settings/__init__.py
+++ recipesite/core/settings/__init__.py
@@ -3,17 +3,18 @@
 Most are defined within base, with some defined conditionally within debug and prod.
 """
 
-import os
+# import os
 
 # from .base import env
-# from .base import *
+from .base import *
 
-DEBUG = bool(os.environ.get("HT_DEBUG", True) == "True")
+# DEBUG = bool(os.environ.get("HT_DEBUG", True) == "True")
 
 try:
     from .local import *  # type: ignore
 except ImportError:
-    if DEBUG:
-        from .dev import *
-    else:
-        from .prod import *
+    pass
+    # if DEBUG:
+    #     from .dev import *
+    # else:
+    #     from .prod import *

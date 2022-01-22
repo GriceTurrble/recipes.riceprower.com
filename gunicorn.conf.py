aa1d7c81c85d897e33068708d43a133c1a372269
Galen Rice <grice@protonmail.com>
2022-01-21 22:12:30 -0500

    feat: moed to Poetry; dockerfile updated
    Becoming increasingly frustrated with managing the project via Pipfile,
    so changing things up to move to Poetry, instead.
    
    Additionally, using `uvicorn` for async workers in the Docker image,
    and added Bandit to the local dev tools (local environment linting).
    
    Docker build updated substantially to attempt to reduce overall size.
    Took advice from some users on the Poetry GitHub to use one stage to
    generate requirements.txt, then copy that file only to a later stage
    for the real installation. Helps to avoid all the Poetry dependencies
    and other build tools lying around in the final image.
    
    Finally, actually *tested* the Docker image on its own, outside
    docker-compose; and found some issues trying to launch it gunicorn.
    Fiddled with the command and the gunicorn config to get things right,
    resulting in the changes made here to gunicorn.conf.py
    and `CMD` in the Dockerfile.
    
    Added template `.docker.env` file, which can be used to copy environment
    variables that should be loaded into the Docker container at runtime.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -8,6 +8,6 @@ bind = "0.0.0.0:" + os.environ.get("PORT", "8000")
 log_file = "-"
 # worker_tmp_dir = "/dev/shm"
 workers = multiprocessing.cpu_count() * 2 + 1
-# worker_class = "uvicorn.workers.UvicornWorker"
+worker_class = "uvicorn.workers.UvicornWorker"
 timeout = 30
 keepalive = 2

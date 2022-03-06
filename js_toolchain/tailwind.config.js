77a535130fedd0341a9815e92a2caa7299c5d0a3
Galen Rice <griceturrble@protonmail.com>
2022-03-06 16:38:59 -0500

    feat: added daisyui to JS tools
    Will start integrating components from there later.

--- js_toolchain/tailwind.config.js
+++ js_toolchain/tailwind.config.js
@@ -1,34 +1,32 @@
-const colors = require('material-ui-colors')
+const colors = require("material-ui-colors");
 module.exports = {
-  content: [
-    '../recipesite/**/*.html',
-  ],
+  content: ["../recipesite/**/*.html"],
   theme: {
     extend: {
       colors: { ...colors },
       screens: {
         // Print utility styles
         // https://tailwindcss.com/docs/breakpoints#styling-for-print
-        'print': {'raw': 'print'},
+        print: { raw: "print" }
       },
       transitionProperty: {
         // Transform property utilities for inset properties
         // https://tailwindcss.com/docs/transition-property#property-values
-        'top': 'top',
-        'left': 'left',
-        'right': 'right',
-        'bottom': 'bottom',
-        'top-left': 'top, left',
-        'top-right': 'top, right',
-        'bottom-left': 'bottom, left',
-        'bottom-right': 'bottom, right',
+        top: "top",
+        left: "left",
+        right: "right",
+        bottom: "bottom",
+        "top-left": "top, left",
+        "top-right": "top, right",
+        "bottom-left": "bottom, left",
+        "bottom-right": "bottom, right"
       },
       zIndex: {
         // Negative value z-index utility
         // https://tailwindcss.com/docs/z-index#negative-values
-        '-10': '-10',
+        "-10": "-10"
       }
-    },
+    }
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")]
+};
